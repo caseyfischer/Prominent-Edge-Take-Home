16e864a40c4305699e0b9e946f25e99191db0e2f
casey rosebridge <caseympfischer@gmail.com>
2024-09-09 15:24:53 -0700

    add google maps map component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 function App() {
-
+    const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string
 
     return (
         <>
-            <h1>Hi!</h1>
+            <APIProvider apiKey={API_KEY}>
+                <Map
+                    style={{ width: '60vw', height: '60vh' }}
+                    defaultCenter={{ lat: 37.5407, lng: -77.4360 }}
+                    defaultZoom={11}
+                    gestureHandling={'greedy'}
+                    disableDefaultUI={true}
+                />
+            </APIProvider>
         </>
     )
 }
